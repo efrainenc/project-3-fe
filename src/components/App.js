b138196656593f8e253248a41ffa74b122409ce4
unknown <efraine387@gmail.com>
2023-01-04 16:11:03 -0500

    Added Register/Login Routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,84 @@
 import './App.css';
 import Header from './Header'
 import Main from './Main'
+import React from 'react'
+import {useState} from 'react'
+import {getUserToken, setUserToken, clearUserToken} from '../utils/authToken'
 
 function App() {
+
+  const [currentUser, setCurrentUser] = useState({})
+
+  // TODO CHECK following state
+  const [isAuthenticated, setIsAuthenticated] = useState(false)
+
+  const registerUser = async (data) => {
+    try {
+      const configs = {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+      const newUser = await fetch(
+        "http://localhost:4000/auth/register",
+        configs
+      )
+
+      const parsedUser = await newUser.json()
+      console.log(parsedUser)
+
+      // sets local storage
+      setUserToken(parsedUser.token)
+      // put the returned user object in state
+      setCurrentUser(parsedUser.currentUser)
+      // adds a boolean cast of the responses isLoggedIn prop
+      // TODO isLoggedIn - was .loggedIn ?
+      setIsAuthenticated(parsedUser.isLoggedIn)
+      
+      return parsedUser
+    } catch (err) {
+      console.log(err)
+      clearUserToken();
+      setIsAuthenticated(false);
+    }
+  }
+
+  const loginUser = async (data) => {
+    try {
+      const configs = {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+      const response = await fetch(
+        "http://localhost:4000/auth/login",
+        configs
+      )
+      const user = await response.json()
+      //console.log(user)
+
+      // sets local storage
+      setUserToken(user.token)
+      // put the returned user object in state
+      setCurrentUser(user.currentUser)
+
+      return user
+    } catch (err) {
+      clearUserToken()
+      setIsAuthenticated(false)
+    }
+  }
+
   return (
     <div className="App">
-      <Header />
-      <Main />
+      <Header user={currentUser}/>
+      <Main signup={registerUser} login={loginUser} user={currentUser} />
     </div>
-  );
+  )
 }
 
 export default App;
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,14 +3,18 @@ import {Routes, Route} from 'react-router-dom'
 import Home from '../pages/Home'
 import Profile from '../pages/Profile'
 import Post from '../pages/Post'
+import RegisterForm from './RegisterForm'
+import LoginForm from './LoginForm'
 
-const Main = (props) => {
+const Main = ({signup, login, user}) => {
   return (
     <main>
       <Routes>
         <Route path='/' element={<Home />}/>
         <Route path='/post' element={<Post />} />
         <Route path='/profile/:id' element={<Profile />}/>
+        <Route path="/register/" element={<RegisterForm signup={signup}/>}/>
+        <Route path="/login/" element={<LoginForm login={login}/>}/>
       </Routes>
     </main>
   )
