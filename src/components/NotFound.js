906e50def64c76d8e0592332ddc9c6fca9d40711
cbowman422 <chrisb0wman@hotmail.com>
2023-01-09 14:25:02 -0500

    404 page

---
+++ src/components/NotFound.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+// Componenent function.
+const NotFound = () => 
+{
+
+  return ( 
+    <div>
+   404 - not Found, please log back in!
+    </div>
+
+       )
+}
+
+export default NotFound
