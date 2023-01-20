f8ac99043b3a4171bbcff8b4ff067286f554b461
unknown <efraine387@gmail.com>
2023-01-19 22:42:30 -0500

    more mobile testing and footer changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,9 +9,9 @@ const Footer = () => { // TODO make an About us page and Contact Us page
   return (
     <footer className='footer'>
       <p onClick={scrollToTop} className="topOfPage">Top of Page</p>
-      <a href='https://github.com/efrainenc/'> About Us</a>
+      {/* <a href='https://github.com/efrainenc/'> About Us</a> */}
       <a href='https://github.com/efrainenc/project-3-fe/tree/main'> Git Hub </a>
-      <a href='https://github.com/cbowman422/'> Contact Us </a>
+      {/* <a href='https://github.com/cbowman422/'> Contact Us </a> */}
       <p> © 2023 FotoBook from The Booleans </p>
     </footer>
   )
